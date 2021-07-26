99933774d622e3d29fbd53949a13ef0cf73eea7e
Wen Zisheng <1921517233@qq.com>
2021-07-26 13:01:56 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -3,5 +3,24 @@
 $.ajaxPrefilter(function(options) {
     // 在发起真正的Ajax请求之前，统一拼接请求的根路径
     options.url = 'http://api-breakingnews-web.itheima.net' + options.url;
-    console.log(options.url);
+
+    // 统一为有权限的接口，设置 headers 请求头
+    if (options.url.indexOf('/my/' !== -1)) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    // 全局统一挂载 complete 回调函数 
+    options.complete = function(res) {
+        // console.log('执行l');
+        // console.log(res);
+        // 在complete回调函数中，可以用res.response.JSON拿到服务器相应回来的数据
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            // 1.强制清空 token
+            localStorage.removeItem('token');
+            // 2.强制跳转到登录页
+            location.href = '/login.html';
+        }
+    }
 })
